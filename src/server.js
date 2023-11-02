ffcb4a5eb126e5e13508ca93c93c253f0186fe40
Zehan Khan <zkhan@hootboard.com>
2023-11-02 20:20:07 +0530

    react added

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const compression = require("compression");
+const path = require("path");
 const app = express();
-const port = process.env.port || 1000;
+const port = process.env.port || 2200;
 
 app.get("/api", (req, res) => {
   res.end("hello from vercel server");
 });
 
-app.get("/", (req, res) => {
-  res.end("server is working");
+app.use(compression());
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", function (req, res) {
+  res.sendFile("/build/index.html", { root: "../" });
 });
 
 app.listen(port, () => {
