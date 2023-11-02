71df916bc148729298481bbfc356191891bf46a2
Zehan Khan <zkhan@hootboard.com>
2023-11-02 20:36:54 +0530

    path fixed

--- src/server.js
+++ src/server.js
@@ -3,15 +3,16 @@ const compression = require("compression");
 const path = require("path");
 const app = express();
 const port = process.env.port || 2200;
+const buildPath = "/../build";
 
+app.use(compression());
+app.use(express.static(path.join(__dirname, buildPath)));
 app.get("/api", (req, res) => {
   res.end("hello from vercel server");
 });
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, "build")));
 app.get("*", function (req, res) {
-  res.sendFile("/build/index.html", { root: "../" });
+  res.sendFile("../build/index.html");
 });
 
 app.listen(port, () => {
